54632f2827321b2a6b872ea45b1c60c59d15a5bc
Mateusz Zawadzki <mateusz.zawadzki91@gmail.com>
2021-04-27 14:07:57 +0200

    working example

---
+++ postgresconnection/postgresconnection/Program.cs
@@ -0,0 +1,23 @@
+﻿using Npgsql;
+using System;
+
+namespace postgresconnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cs = "Host=127.0.0.1;Username=testuser@test.com;Password=test123!@#;Database=testdb";
+
+            using var con = new NpgsqlConnection(cs);
+            con.Open();
+
+            var sql = "SELECT version()";
+
+            using var cmd = new NpgsqlCommand(sql, con);
+
+            var version = cmd.ExecuteScalar().ToString();
+            Console.WriteLine($"PostgreSQL version: {version}");
+        }
+    }
+}
